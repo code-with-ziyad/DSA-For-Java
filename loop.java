9e8881681ea3e478fada5b8698736dac1bcbbadd
ZIYAD AFTAB <ziyadyt94@gmail.com>
2025-06-23 21:19:41 +0500

    do while loop in java

--- loop.java
+++ loop.java
@@ -1,14 +1,15 @@
 public class loop {
      public static void main(String[] args){
        
-        // while loop
+        // do while loop
         
-        int i = 0;
-        while(i<=111){
-            System.out.println(i);
-            i++;
-        }    
-      
+        int j = 0;
+      do{
+        System.out.println(j);
+      j = j + 1;
+    }
+   while(j<=100);
+    
         }
 
      }
