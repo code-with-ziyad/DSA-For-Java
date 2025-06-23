ce51c857de683ede98256177c4ccc041a1aab242
ZIYAD AFTAB <ziyadyt94@gmail.com>
2025-06-23 20:58:38 +0500

    for loop

---
+++ loop.java
@@ -0,0 +1,11 @@
+public class loop {
+     public static void main(String[] args){
+       
+        // for loop
+        
+        for(int x = 0; x < 50;  x = x + 1){
+            System.out.println("Ziyad Aftab");
+        }
+
+     }
+}
