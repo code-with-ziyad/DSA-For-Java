8cd23c0fb884d6111567d496d29d7a174f92f06d
ZIYAD AFTAB <ziyadyt94@gmail.com>
2025-06-25 20:41:35 +0500

    use scanners and loop pattern

--- patterns.java
+++ patterns.java
@@ -1,12 +1,25 @@
-public class patterns {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        for(int i = 1; i <=10; i++){
-            for(int j =1; j<=50; j++)
-            System.out.print("*");
-            System.out.println();
+public class patterns {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Input Square Rows: ");
+   
   
+
+    int iLimit = sc.nextInt();
+        System.out.println("Input Square Colums ");
+int jLimit = sc.nextInt();
+    for (int i = 0; i <= iLimit; i++) {
+   
+      
+      for (int j = 0; j <= jLimit; j++)
+        System.out.print("* ");
+      System.out.println();
+
+    
     }
-        
-    }    
+  sc.close();
+  }
 }
