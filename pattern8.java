4f68b9a840fc5130417d03ffa42412871bf00366
ZIYAD AFTAB <ziyadyt94@gmail.com>
2025-08-07 22:05:10 +0500

    Add files via upload

---
+++ pattern8.java
@@ -0,0 +1,20 @@
+ public class pattern8{
+    public static void main (String[] args) {
+
+      int n = 6;  // number of row
+      int Number = 1;  // number staRT WITH 1
+
+for(int i = 1; i<=n; i++){  // Outer loop 
+   for(int j = 1; j<=i; j++){  // inner loop
+  System.out.print(Number);   // print number 
+  Number++;                  // number increase
+   }
+   System.out.println();     // line change
+}
+
+
+
+
+    }
+   }
+
