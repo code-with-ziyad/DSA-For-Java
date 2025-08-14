2d5151e1f8dd76668d5da77a03c830c4dac05ef6
ZIYAD AFTAB <ziyadyt94@gmail.com>
2025-08-14 22:35:44 +0500

    Add files via upload

---
+++ pattern14.java
@@ -0,0 +1,32 @@
+public class pattern14 {
+    public static void main(String[] args) {
+        
+        int n =3;
+        for (int i = 0; i <= n; i++){
+            for (int j = 0; j <=n-i; j++) {
+                System.out.print(" ");
+            }
+           for (int k = 0; k <=i; k++) {
+               System.out.print("*");
+           }
+              for (int k = 1; k <=i; k++) {
+               System.out.print("*");
+              }
+              System.out.println();
+        }
+      for (int i = n-1; i >= 0; i--){
+            for (int j = 0; j <=n-i; j++) {
+                System.out.print(" ");
+            }
+           for (int k = 0; k <=i; k++) {
+               System.out.print("*");
+           }
+              for (int k = 1; k <=i; k++) {
+               System.out.print("*");
+              }
+              System.out.println();
+        }
+    
+   
+    }
+}
