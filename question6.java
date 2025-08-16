f6ea6900423e277fcf00280700bd7aba5e7c9ed1
ZIYAD AFTAB <ziyadyt94@gmail.com>
2025-08-16 16:50:43 +0500

    Q6:Write an infinite loop using do while condition.
    Write an infinite loop using do while condition.

---
+++ question6.java
@@ -0,0 +1,10 @@
+public class question6 {
+    public static void main(String[] args) {
+        
+   int a = 0;
+        do{
+            System.out.println(a);
+            a++;
+        }while(true);
+    }
+}
