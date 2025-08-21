8015665c778a963e02896d3a13b1d1cf2c1f93a0
ZIYAD AFTAB <ziyadyt94@gmail.com>
2025-08-21 10:45:12 +0500

    Add files via upload

---
+++ classarray.java
@@ -0,0 +1,17 @@
+public class classarray {
+    public static void main(String[] args) {
+    //     int[] num ; // Declaring array
+      int[]  num = new int [5]; //creating array
+    //   float [] num1 = new float[5];  // D+C
+     int[] arr = {1,2,3,4,5};
+     int find  = 2; 
+       System.out.println("this are numbers");
+     for (int i = 0; i <= arr.length; i++) {
+        //  System.out.println(arr[i]);
+       if (arr == find[i]) {
+        
+       }
+
+     }
+    }
+}
