b3ed10ef2db3a248f6729c021b099e22c32b0b6c
ZIYAD AFTAB <ziyadyt94@gmail.com>
2025-08-21 10:44:53 +0500

    university

---
+++ string.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class string {
+    public static void main(String[] args) {
+        Scanner str = new Scanner(System.in);
+        System.out.print("Enter your Department = ");
+        String line = str.nextLine();
+        System.out.println(line);
+    }
+}
