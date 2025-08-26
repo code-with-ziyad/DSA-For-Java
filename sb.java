e3900b9945195d40aa9e2985b37282e81e29a06c
ZIYAD AFTAB <ziyadyt94@gmail.com>
2025-08-26 22:03:31 +0500

    Add files via upload

---
+++ sb.java
@@ -0,0 +1,23 @@
+public class sb {
+    public static void main(String[] args) {
+        
+        StringBuilder sb = new StringBuilder("Ziyad Ahmed");
+    //   sb.reverse();       // ye string ko reverse karta hai 
+    //   System.out.println(sb);
+
+    //   sb.insert(5, "Z");                 // ye string ke ksi bi index me kch bi add karta hai
+    //   System.out.println(sb);
+
+        // sb.append( " baloch");     // string ke end me kch bi add kardega 
+        // System.out.println(sb);
+
+        // sb.setCharAt(1, '!');    // ye string ke ksi bi character ko replace kardega 
+        // System.out.println(sb);
+
+        // sb.insert(7, 'B');  // ye ksi bi index ke beech me jake kch bi add kardega 
+        // System.out.println(sb);
+
+        // sb.delete(5, 10);       // ye string me se kch bi delete kardega
+        // System.out.println(sb);
+    }
+}
